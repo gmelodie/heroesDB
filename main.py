7d4453dcf6934f1a5da1065cde9747858b416d13
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-11-22 13:16:45 -0300

    well crap.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import mpyq
+import pprint
 import heroprotocol
 from heroprotocol.versions import build, latest
 
 NUMARGS=2
-
+TESTING=False
+pp = pprint.PrettyPrinter(indent=4)
 
 def get_details(full_filename):
     archive = mpyq.MPQArchive(full_filename)
@@ -29,18 +31,111 @@ def get_details(full_filename):
 
 
 def extract_player_details(player):
-    nick = player['m_name'].decode()
+    # remove weird characters from username
+    nick = player['m_name'].decode('ascii', 'ignore').lower()
+
     player_id = player['m_toon']['m_id']
     hero = player['m_hero'].decode()
 
     # result ranks the teams (1 won, 2 lost)
     result = player['m_result']
+    win = False
     if result == 1:
         win = True
-    elif result == 2:
-        win = False
 
     return nick, player_id, hero, win
+
+
+def get_players_stats(directory):
+    players = {}
+    for i, filename in enumerate(os.listdir(directory)):
+        if filename.endswith(".StormReplay"):
+            full_filename = directory+'/'+filename
+            details = get_details(full_filename)
+            for player in details['m_playerList']:
+                nick, player_id, hero, win = extract_player_details(player)
+
+                # update overall statistics
+                if nick not in players:
+                    players[nick] = {'wins': 0,\
+                                     'total_games': 0,\
+                                     'win_rate': 0,\
+                                     'heroes': {},\
+                                     'games': {}}
+                if hero not in players[nick]['heroes']:
+                    players[nick]['heroes'][hero] = {"total_games": 0,\
+                                                     "wins": 0,\
+                                                     "win_rate": 0}
+
+                players[nick]['heroes'][hero]['total_games'] += 1
+                players[nick]['total_games'] += 1
+                if win:
+                    players[nick]['heroes'][hero]['wins'] += 1
+                    players[nick]['wins'] += 1
+
+                # update overall win rate
+                wins = players[nick]['wins']
+                total_games = players[nick]['total_games']
+                win_rate = int(100*wins/total_games)
+                players[nick]['win_rate'] = win_rate
+
+                # update per-hero win rate
+                wins = players[nick]['heroes'][hero]['wins']
+                total_games = players[nick]['heroes'][hero]['total_games']
+                win_rate = int(100*wins/total_games)
+                players[nick]['heroes'][hero]['win_rate'] = win_rate
+
+                # update map statistics
+                map_title = details['m_title'].decode()
+                if map_title not in players[nick]['games']:
+                    players[nick]['games'][map_title] = {'wins': 0, \
+                                                         'total_games': 0, \
+                                                         'win_rate': 0,
+                                                         'heroes': {}}
+                if hero not in players[nick]['games'][map_title]['heroes']:
+                    players[nick]['games'][map_title]['heroes'][hero] = 0
+
+                players[nick]['games'][map_title]['heroes'][hero] += 1
+                players[nick]['games'][map_title]['total_games'] += 1
+                if win:
+                    players[nick]['games'][map_title]['wins'] += 1
+
+                # update per-map win rate
+                wins = players[nick]['games'][map_title]['wins']
+                total_games = players[nick]['games'][map_title]['total_games']
+                win_rate = int(100*wins/total_games)
+                players[nick]['games'][map_title]['win_rate'] = win_rate
+
+        print(f'Loading files: {i}')
+    if TESTING:
+        pp.pprint(players)
+    return players
+
+
+def print_overall_stats(player):
+    print(f'================ STATS ================')
+    print(f"Overall win rate: {player['win_rate']}%")
+    print()
+
+
+def print_heroes(heroes):
+    print(f'================ HEROES ================')
+    for hero, stats in heroes:
+        print(f"{stats['total_games']}\t{stats['win_rate']}%\t-> {hero}")
+    print()
+
+
+def print_maps(maps):
+    print(f'================ MAPS ================')
+    for map_title, stats in maps:
+        print(f"{stats['win_rate']}%\t{map_title}")
+        sorted_heroes = sorted(stats['heroes'].items(), \
+                reverse=True, key=lambda item: item[1])
+
+        for hero, ngames in sorted_heroes:
+            print(f'\t{ngames} -> {hero}')
+        print()
+    print()
 
 
 if __name__ == '__main__':
@@ -48,20 +143,31 @@ if __name__ == '__main__':
         print('usage: python3 main.py <REPLAY_FOLDER>')
         exit(1)
 
-    directory = sys.argv[1]
+    replays_directory = sys.argv[1]
 
     # make sure directory name doesn't have a trailing /
-    if directory.endswith('/'):
-        directory = directory[:-1]
+    if replays_directory.endswith('/'):
+        replays_directory = replays_directory[:-1]
 
-    for filename in os.listdir(directory):
-        if filename.endswith(".StormReplay"):
-            full_filename = directory+'/'+filename
-            details = get_details(full_filename)
-            for player in details['m_playerList']:
-                nick, player_id, hero, win = extract_player_details(player)
-                if win:
-                    print(f"{nick}(ID: {player_id}) ---> {hero}\t[WIN]")
-                else:
-                    print(f"{nick}(ID: {player_id}) ---> {hero}\t[LOSE]")
-        break # TODO: remove, using for testing
+    players = get_players_stats(replays_directory)
+
+    query_nick = ""
+    while query_nick != 'exit':
+        query_nick = input("Player: ")
+        if query_nick not in players:
+            print('[ERROR] Player not found')
+            continue
+
+        sorted_maps = sorted(players[query_nick]['games'].items(), \
+            reverse=True, key=lambda item: item[1]['win_rate'])
+        sorted_heroes = sorted(players[query_nick]['heroes'].items(), \
+            reverse=True, key=lambda item: item[1]['total_games'])
+
+        if TESTING:
+            pp.pprint(players[query_nick])
+            break
+        print_overall_stats(players[query_nick])
+        print_heroes(sorted_heroes)
+        print_maps(sorted_maps)
+        print(f"Suggested ban: {sorted_heroes[0][0]}")
+        print()
